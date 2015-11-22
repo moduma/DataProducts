98c7ff244d7245a4ff54819061144d8f66ba4437
unknown <Oduma@HDQ37888.kq.kenya-airways.com>
2015-11-22 23:23:29 +0300

    Adding Presentation

---
+++ helper.R
@@ -0,0 +1,13 @@
+##Investigation of Exponential Distribution
+The exponential distribution can be simulated in R with rexp(n, lambda) where lambda is the rate parameter. The mean of exponential distribution is given by  1/lambda and the standard deviation is also given by 1/lambda. In this application, Lambda was set to 0.2 for all of the simulations. Also, to enable reproducibility, seed was set to 1000.
+The application allows you to vary the number of obserevations to be averaged at any given point. This is done by the slider bar. 
+The application also has a slider to enable you vary the number of simulations 
+
+##Outputs
+As the sliders for the number of simulations and the number of observations are varied, a histogram is plotted accordingly.
+The values for the theoretical mean and the sample mean are given as outputs. Comparison of the expected standard deviation and the theoretical standard deviation is also give.
+
+##Obsevations
+It can be observed that as the number of obsevations and simulations increase, the plotted histogram becomes uniformly distributed. The sample mean and the theoretical mean also tends to be the same as the number of obsevations and simulations increase. It is also observed that the expected standard deviation and the theoretical standard deviation tends to be the same as the number of simulations and observations increases.
+
+
